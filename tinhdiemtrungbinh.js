a16a13175177b2507ea398e8c3c35c77f71c0700
Hieu.Le <letrunghieu2602@gmail.com>
2020-03-09 21:18:18 +0700

    edits xong

--- tinhdiemtrungbinh.js
+++ tinhdiemtrungbinh.js
@@ -2,9 +2,11 @@ function ketQua() {
     let physical = document.getElementById("physical").value;
     let chemistry = document.getElementById("chemistry").value;
     let biological = document.getElementById("biological").value;
-    If (physical > 0 && chemistry > 0 && biological > 0){
+    if (physical > 0 && chemistry > 0 && biological > 0) {
         let total = Number(physical) + Number(chemistry) + Number(biological);
         document.getElementById("total").innerHTML = total.toFixed(2);
+    }
+    if (physical > 0 && chemistry > 0 && biological > 0) {
         let average = (Number(physical)+Number(chemistry)+Number(biological))/3;
         document.getElementById("average").innerHTML = average.toFixed(2);
     }
